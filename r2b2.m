0d412492b1d0a8d0a7c9713ea63f0bf330663f4f
Max Fisher <f.mach4@gmail.com>
2016-10-28 11:12:26 +1100

    Update r2b2 to use the new class structure

--- r2b2.m
+++ r2b2.m
@@ -1,76 +1,52 @@
 % r2b2.m
 % Author: Max Fisher
 
-function r2b2(wav_name, output_directory)
-%%%%%%%%%%%%%%%
-%% (independently) tweakable parameters
-%%%%%%%%%%%%%%%
+function r2b2(audio_filename, audio_directory, data_output_directory)
+	if nargin < 2
+		audio_directory = '.';
+	end
+	if nargin < 3
+		output_data = 0;
+	else
+		output_data = 1;
+	end
+	
+	audio_file_path = strcat(audio_directory, '/', audio_filename);
+	[audio_data, audio_sample_rate] = audioread(audio_file_path);
+	
+	% give audio to feature class
+	feature1 = odf_klapuri;
+	feature1.initialise(audio_data, audio_sample_rate, ...
+		'klapuris_feature');
+	
+	feature1.compute_feature;
+	
+	% make a tempo_phase estimator for each channel of feature1, since
+	% each channel is analysed independently
+	tp_estimator = cell(feature1.num_feature_channels);
+	
 
-% fix up to trim off extension
-WAV_NAME = wav_name;
-WAV_EXT = '.wav';
+	
+	for n = 1:feature1.num_feature_channels
+		tp_estimator{n} = tpe_autocorrelation;
+		tp_estimator{n}.initialise(feature1.feature_matrix(:, n), ...
+			feature1.feature_sample_rate, sprintf('acf-feature1-ch%d', n));
+	end
+	
+	% pick some sample frames to plot
+	sample_frames = round(linspace(1, tp_estimator{1}.num_feature_frames, 4));
+	sample_frames = [sample_frames(2:end-1)];
+	
+	for n = 1:feature1.num_feature_channels
+		tp_estimator{n}.compute_tempo_phase_estimates;
+		tp_estimator{n}.plot_sample_intermediate_data(sample_frames);
+		
+		if output_data
+			tp_estimator{n}.output_tp_estimates(data_output_directory);
+		end
+	end
+		
+	
 
-% assume mono audio wav file in current dir.
-
-[y, Fs] = audioread(strcat('./', WAV_NAME, WAV_EXT));
-
-%%%%%%%%%%%%
-%% Calculate onset detection function
-%%%%%%%%%%%%%
-
-[df, FEATURE_RATE] = odf_klapuri(y, fs);
-
-
-
-
-feature_frame = @(g, n) g((n-1)*FEATURE_HOP_SIZE + 1: min(((n-1)*FEATURE_HOP_SIZE + FEATURE_WIN_LENGTH), end), :);
-
-feature_time_axis = (1:FEATURE_WIN_LENGTH)/FEATURE_RATE;
-
-sample_frames = round(linspace(1, NUM_FEATURE_FRAMES, 4));
-sample_frames = sample_frames(2:end-1);
-
-%sample_frames = [sample_frames ; sample_frames + 1];
-
-% use to disable plotting;
-%sample_frames = [];
-
-
-%%%%%%%%%%%%%%%%%%%%
-%% Processing of feature vector for periodicity
-% In reality everything would be done in the previous loop,
-% also tempo estimates would have to be made for past feature measurements
-%%%%%%%%%%%%%%%%%%%%%%
-
-
-tempo_estimates = cell(NUM_FEATURE_FRAMES, NUM_FEATURE_CHANNELS);
-tempo_confidences = cell(NUM_FEATURE_FRAMES, NUM_FEATURE_CHANNELS);
-beat_alignment_estimates = cell(NUM_FEATURE_FRAMES, NUM_FEATURE_CHANNELS);
-beat_alignment_confidences = cell(NUM_FEATURE_FRAMES, NUM_FEATURE_CHANNELS);
-
-NUM_TEMPO_ESTIMATES = 4;
-
-tic;
-
-time = toc;
-fprintf('spent %f s calculating beat_alignments\n', time);
-
-%%%%%%%%%%%%%%%%%%%%
-%% Write beat data to files
-%%%%%%%%%%%%%%%%%%%%%
-tic;
-
-% export data in the following format
-% one file per feature channel
-for c = 1:NUM_FEATURE_CHANNELS;
-    filename_for_channel = strcat('./', WAV_NAME, BEAT_DATA_OUTPUT_SUFFIX, ...
-        sprintf('-channel-%d.txt', c));
-    outfile = fopen(filename_for_channel, 'w+');
-
-    end;
-end;
-
-time = toc;
-fprintf('spent %f s writing beat data\n', time);
-
-% vim: tabstop=4 expandtab
+	
+	
