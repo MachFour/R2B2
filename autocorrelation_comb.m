045af0a08478449928106b11f8ef80ee9c379109
Max Fisher <f.mach4@gmail.com>
2016-11-06 00:04:36 +1100

    playing around with autocorrelation comb

--- autocorrelation_comb.m
+++ autocorrelation_comb.m
@@ -3,21 +3,34 @@
 
 % Author: Max Fisher
 
-function comb = autocorrelation_comb(length, lag_samples)
+function comb = autocorrelation_comb(len, lag_samples)
 % make impulse matrix to estimate 
 
 %prior tempo weighting function (rayleigh distribution)
 % tempo lag in samples
 beta = 60/120; % prior has maximum at 120BPM
 tempo_weighting = @(tempo_lag) tempo_lag/beta^2*exp(-tempo_lag^2/(2*beta^2));
-comb = zeros(length, 1);
-NUM_COMBS = 3;
+comb = zeros(len, 1);
+num_combs = floor((len-3)/lag_samples);
 
-% tempo lag varies from 1:FEATURE_WIN_LENGTH/4 samples
-for p = 1:NUM_COMBS
-	comb(1+lag_samples*p) = 2;
-	comb(1+lag_samples*p - 1) = 1;
-	comb(1+lag_samples*p + 1) = 1;
+if num_combs < 2
+	warning('less than 2 combs can fit');
+end
+
+for p = 1:num_combs
+	comb(1+lag_samples*p -2) = 1;
+	comb(1+lag_samples*p -1) = 4;
+	comb(1+lag_samples*p +0) = 9;
+	comb(1+lag_samples*p +1) = 4;
+	comb(1+lag_samples*p +2) = 1;
+end
+
+% ensure comb has correct length (should not be needed)
+
+if length(comb) > len
+	warning('correcting comb length from %d to %d', ...
+		length(comb), len);
+	comb = comb(1:len);
 end
 
 % normalise to have sum 1
