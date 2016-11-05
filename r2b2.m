6d09a2d4c91089b7d142f7c46e6ba1adc032446f
Max Fisher <f.mach4@gmail.com>
2016-11-06 01:00:15 +1100

    update r2b2 to use autocorrelation2, add 3D scatter plot

--- r2b2.m
+++ r2b2.m
@@ -29,21 +29,50 @@ function r2b2(audio_filename, audio_directory, data_output_directory)
 	tp_estimator = cell(feature1.num_feature_channels, 1);
 
 	for n = 1:feature1.num_feature_channels
-		tp_estimator{n} = tpe_autocorrelation;
+		tp_estimator{n} = tpe_autocorrelation2;
 		tp_estimator{n}.initialise(feature1.feature_matrix(:, n), ...
 			feature1.feature_sample_rate, sprintf('acf-feature1-ch%d', n));
 	end
 
 	% pick some sample frames to plot
-	sample_frames = round(linspace(1, tp_estimator{1}.num_feature_frames, 4));
-	sample_frames = [sample_frames(2:end-1)];
+	% plot all the frames!
+	num_sample_frames = tp_estimator{1}.num_feature_frames/10;
+	sample_frames = round(linspace(1, ...
+		tp_estimator{1}.num_feature_frames, num_sample_frames+2));
+	%plot in reverse order so they'll appear on top of each other in
+	%correct order
+	sample_frames = [sample_frames(end-1:-1:1)];
+	disp('Sample frames:'); disp(sample_frames);
 
 	for n = 1:feature1.num_feature_channels
 		tp_estimator{n}.compute_tempo_phase_estimates;
 		if output_data
 			tp_estimator{n}.output_tempo_phase_data(data_output_directory);
-		else
-			tp_estimator{n}.plot_sample_intermediate_data(sample_frames);
+		end
+	end
+
+	if output_data == 0
+		% do scatter plot of all estimates in sample frames
+		for k = sample_frames
+			figure; hold on;
+			for n = 1:feature1.num_feature_channels
+				estimates_n = tp_estimator{n}.tempo_phase_estimates{k};
+				%plot in seconds
+				if isempty(estimates_n)
+					warning('No estimates for feature %d in frame %d', n, k);
+				else
+					feature_fs = tp_estimator{n}.feature_sample_rate;
+					tempos = estimates_n(:, 1);
+					phases = estimates_n(:, 3);
+					confidences = estimates_n(:, 4);
+					stem3(tempos/feature_fs, phases/feature_fs, confidences, 'filled');
+				end
+
+			end
+			title(sprintf('Scatterplot of tempo/phase estimates at %.2f s', ...
+				tp_estimator{1}.get_frame_end_time(k)));
+			xlabel('Tempo period (seconds)');
+			ylabel('Offset from frame end (seconds)');
 		end
 	end
 end
