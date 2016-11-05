431e47316451b34702d6f0366cffc3e18c50a9e0
Max Fisher <f.mach4@gmail.com>
2016-11-05 19:46:26 +1100

    attempt to improve phase confidence measure

--- beat_alignment_function.m
+++ beat_alignment_function.m
@@ -3,15 +3,19 @@
 % of beats in a frame of an onset detection function.
 % beat spacing is given in samples.
 
-% Author: Max Fisher
-function f = beat_alignment_function(frame, tempo_spacing)
-	frame_length = length(frame);
-	% normalise frame data
-	mean = sum(frame)/frame_length;
-	variance = sum(frame.^2)/frame_length;
+% frame should be a row vector
+% returns a row vector
 
-	% (we reverse frame to examine most recent onsets first)
-	frame = (fliplr(frame) - mean)/sqrt(variance);
+% Author: Max Fisher
+function baf = beat_alignment_function(frame, tempo_spacing)
+	frame_length = length(frame);
+
+	% make mean zero but keep variance?
+	% have to think about this
+	%frame = frame - mean(frame);
+	
+	% we reverse frame to examine most recent onsets first
+	frame = fliplr(frame);
 	SPIKE_WIDTH = 2;
 
 	impulse_train = zeros(1, frame_length);
@@ -24,24 +28,32 @@ function f = beat_alignment_function(frame, tempo_spacing)
 
 	% exponential decay of weighting:
 	% w(n) = a^(-a*n/N)
-	% choose a = 2
-	impulse_weighting = 2.^(-2/frame_length*(1:frame_length));
+	%choose a = 2
+	a = 2;
+	impulse_weighting = a.^(-a/frame_length*(1:frame_length));
 	impulse_train = impulse_train.*impulse_weighting;
-	%figure; plot(impulse_train); hold on; plot(frame);
-	%title(sprintf('Reversed feature frame and impulses for comb width %d', tempo_spacing));
-	%xlabel('Samples');
+
+	% normalise impulse train to have sum 1,
+	% so it has the effect of a weighted sum
+	% of odf samples
+	impulse_train = impulse_train/sum(impulse_train);
+
+%  	figure; plot(impulse_train); hold on; plot(frame);
+%  	title(sprintf('Reversed feature frame and impulses for comb width %d', tempo_spacing));
+%  	xlabel('Samples');
 
 	% only makes sense to consider shifts up to tempo_spacing, since
 	% beyond that point the comb 'teeth' would fall off the end
 
-	f = xcorr(frame, impulse_train, tempo_spacing);
-	%figure; plot(f);
-	%title('Beat alignment function');
-	%xlabel('Comb shift (samples)');
-	%ylabel('Strength');
+	baf = zeros(1, tempo_spacing);
+	for shift = 0:tempo_spacing-1
+		shifted_frame = circshift(frame, [0, -shift]); 
+		baf(shift+1) = sum(shifted_frame.*impulse_train);
+	end;
 
-	% return a vector of tempo_spacing length with zero lag as the first
-	% sample
-	f = circshift(f, [length(f), 0]);
-	f = f(1:tempo_spacing);
+% 	figure; plot(baf);
+% 	title(sprintf('Beat alignment function - tempo spacing = %d', tempo_spacing));
+% 	xlabel('Comb shift (samples)');
+% 	ylabel('Strength');
+
 end
